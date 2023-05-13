460d79b91fffe0cc61f54ea2d262d63ff4d01011
Fauzii <113253689+FauziSlebew@users.noreply.github.com>
2023-05-14 07:33:10 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://luitutolol103:FAUZI123@cluster0.7kxpe7c.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@11" //isi apa aja bebas
 global.your_email = "luitutolol103@gmail.com" //email
-global.email_password = "" //application password email
+global.email_password = "ywdhsih182@gmail.com" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
